3f36322b3564604a0752569a662ccbacc9400b19
Victory Ifebhor <victory.ifebhor@gmail.com>
2022-04-08 11:03:19 +0100

    Moved the tests to a package outside the APIs' package

---
+++ tests/test_user.py
@@ -0,0 +1,18 @@
+from fastapi.testclient import TestClient
+
+from APIs.routers import user
+
+client = TestClient(user.router)
+
+
+def test_register():
+    response = client.post(
+        "/register",
+        json={
+            "username": "admin",
+            "full_name": "John Doe",
+            "staff_type": "Inventory Staff",
+        },
+    )
+    assert response.status_code == 200
+    assert response.json() == {"detail": "User account created."}
