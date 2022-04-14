b7bf0608adfbc2296b488f558a4356191c075b53
Victory Ifebhor <victory.ifebhor@gmail.com>
2022-04-14 17:38:15 +0100

    Created function to check user has permission to view orders

--- APIs/app/auth/permissions.py
+++ APIs/app/auth/permissions.py
@@ -5,13 +5,25 @@ from models.user import User as ModelUser
 from schemas.user import Staff
 
 
+unauthorized_error = HTTPException(
+    status_code=status.HTTP_403_FORBIDDEN,
+    detail="Not authorized to perform this action"
+)
+
+
 def ensure_is_admin(
     current_user: ModelUser = Depends(AuthHandler.auth_wrapper)
 ):
     if current_user.staff != Staff.ADMIN:
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail="Not authorized to perform this action"
-        )
+        raise unauthorized_error
+
+    return current_user
+
+
+def ensure_can_view_order(
+    current_user: ModelUser = Depends(AuthHandler.auth_wrapper)
+):
+    if current_user.staff not in (Staff.CASHIER, Staff.KITCHEN_STAFF):
+        raise unauthorized_error
 
     return current_user
