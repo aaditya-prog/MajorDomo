1837f52d6e1d86aa82dfe3327ca030a4202985e6
Aaditya Dulal <artdityadulal@gmail.com>
2022-04-16 02:06:52 +0545

    Remove Dead Code :coffin:

--- APIs/app/config/settings.py
+++ APIs/app/config/settings.py
@@ -28,28 +28,6 @@ class Settings(BaseSettings):
         f"@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
     )
 
-    # Email Credentials
-    MAIL_USERNAME: Optional[str] = os.getenv("MAIL_USERNAME", "")
-    MAIL_PASSWORD: Optional[str] = os.getenv("MAIL_PASSWORD", "")
-    MAIL_FROM: Optional[EmailStr] = EmailStr(os.getenv("MAIL_FROM", ""))
-    MAIL_PORT: Optional[int] = int(os.getenv("MAIL_PORT", ""))
-    MAIL_SERVER: Optional[str] = os.getenv("MAIL_SERVER", "")
-    MAIL_FROM_NAME: Optional[str] = os.getenv("MAIN_FROM_NAME", "")
-
-    # Email Configurations
-    conf = ConnectionConfig(
-        MAIL_USERNAME=MAIL_USERNAME,
-        MAIL_PASSWORD=MAIL_PASSWORD,
-        MAIL_FROM=MAIL_FROM,
-        MAIL_PORT=MAIL_PORT,
-        MAIL_SERVER=MAIL_SERVER,
-        MAIL_FROM_NAME=MAIL_FROM_NAME,
-        MAIL_TLS=True,
-        MAIL_SSL=False,
-        USE_CREDENTIALS=True,
-        TEMPLATE_FOLDER=Path("app/templates")
-    )
-
 
 # Using the @lru_cache() decorator on top,
 # the Settings object will be created only once, the first time it's called.
