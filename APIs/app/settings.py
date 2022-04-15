31b6cbe1033a72859ba8fe0ef340decddc95ac0f
Aaditya Dulal <artdityadulal@gmail.com>
2022-04-15 16:48:43 +0545

    Remove Dead Code :coffin:

--- APIs/app/settings.py
+++ APIs/app/settings.py
@@ -5,7 +5,6 @@ from pathlib import Path
 from typing import Optional
 
 from dotenv import load_dotenv
-from fastapi_mail import ConnectionConfig
 from pydantic import BaseSettings, EmailStr
 
 # Utilizing dotenv to load environment variables from the OS.
@@ -27,28 +26,6 @@ class Settings(BaseSettings):
     DATABASE_URL = (
         f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}"
         f"@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
-    )
-
-    # Email Credentials
-    MAIL_USERNAME: Optional[str] = os.getenv("MAIL_USERNAME", "")
-    MAIL_PASSWORD: Optional[str] = os.getenv("MAIL_PASSWORD", "")
-    MAIL_FROM: Optional[EmailStr] = EmailStr(os.getenv("MAIL_FROM", ""))
-    MAIL_PORT: Optional[int] = int(os.getenv("MAIL_PORT", ""))
-    MAIL_SERVER: Optional[str] = os.getenv("MAIL_SERVER", "")
-    MAIL_FROM_NAME: Optional[str] = os.getenv("MAIN_FROM_NAME", "")
-
-    # Email Configurations
-    conf = ConnectionConfig(
-        MAIL_USERNAME=MAIL_USERNAME,
-        MAIL_PASSWORD=MAIL_PASSWORD,
-        MAIL_FROM=MAIL_FROM,
-        MAIL_PORT=MAIL_PORT,
-        MAIL_SERVER=MAIL_SERVER,
-        MAIL_FROM_NAME=MAIL_FROM_NAME,
-        MAIL_TLS=True,
-        MAIL_SSL=False,
-        USE_CREDENTIALS=True,
-        TEMPLATE_FOLDER=Path("./templates")
     )
 
 
