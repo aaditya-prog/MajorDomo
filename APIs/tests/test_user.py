ec9b2b749c9c252a9181aacab18506630e98f99e
Victory Ifebhor <victory.ifebhor@gmail.com>
2022-04-15 23:30:12 +0100

    Updated the test client to accept the instance of the main FastAPI instance

--- APIs/tests/test_user.py
+++ APIs/tests/test_user.py
@@ -1,7 +1,20 @@
 from fastapi.testclient import TestClient
 
-from ..app.routers import user
+from ..app.main import app
 
-client = TestClient(user.router)
+client = TestClient(app)
 
 
+def test_user_registration_route_is_protected():
+    response = client.post(
+        "/user/register",
+        json={
+            "username": "admin",
+            "full_name": "John Doe",
+            "staff_type": "Inventory Staff",
+        },
+    )
+    assert response.status_code == 403
+    assert response.json() == {
+        "detail": "Not authenticated"
+    }
