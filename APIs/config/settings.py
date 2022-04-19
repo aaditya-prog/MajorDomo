a370c8b0a1f3e9893e6a827f6b3549e29fec8e32
Aaditya Dulal <artdityadulal@gmail.com>
2022-04-19 16:29:17 +0545

    DB Settings for deployment

--- APIs/config/settings.py
+++ APIs/config/settings.py
@@ -1,4 +1,5 @@
 import os
+import urllib
 
 from functools import lru_cache
 from pathlib import Path
@@ -16,16 +17,27 @@ class Settings(BaseSettings):
     # Project Details
     PROJECT_NAME: Optional[str] = "FastAPI Restaurant Management System(RMS)"
     PROJECT_VERSION: Optional[str] = "1.0.0"
+    #
+    # # Database Configuration Settings
+    # POSTGRES_USER: Optional[str] = os.getenv("POSTGRES_USER")
+    # POSTGRES_PASSWORD: Optional[str] = os.getenv("POSTGRES_PASSWORD")
+    # POSTGRES_SERVER: Optional[str] = os.getenv("POSTGRES_SERVER", "localhost")
+    # POSTGRES_PORT: Optional[str] = os.getenv("POSTGRES_PORT")
+    # POSTGRES_DB: Optional[str] = os.getenv("POSTGRES_DB")
+    # SSL_MODE: Optional[str] = os.getenv("SSL_MODE")
+    # DATABASE_URL = (
+    #     f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}?sslmode={SSL_MODE}"
+    # )
 
-    # Database Configuration Settings
-    POSTGRES_USER: Optional[str] = os.getenv("POSTGRES_USER")
-    POSTGRES_PASSWORD: Optional[str] = os.getenv("POSTGRES_PASSWORD")
-    POSTGRES_SERVER: Optional[str] = os.getenv("POSTGRES_SERVER", "localhost")
-    POSTGRES_PORT: Optional[str] = os.getenv("POSTGRES_PORT")
-    POSTGRES_DB: Optional[str] = os.getenv("POSTGRES_DB")
-    SSL_MODE: Optional[str] = os.getenv("SSL_MODE")
+    host_server = os.environ.get('host_server', 'localhost')
+    db_server_port = urllib.parse.quote_plus(str(os.environ.get('db_server_port', '5432')))
+    database_name = os.environ.get('database_name', 'fastapi')
+    db_username = urllib.parse.quote_plus(str(os.environ.get('db_username', 'postgres')))
+    db_password = urllib.parse.quote_plus(str(os.environ.get('db_password', 'secret')))
+    ssl_mode = urllib.parse.quote_plus(str(os.environ.get('ssl_mode', 'prefer')))
+
     DATABASE_URL = (
-        f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}?sslmode={SSL_MODE}"
+        f"postgresql://{db_username}:{db_password}@{db_password}:{db_server_port}/{database_name}?sslmode={ssl_mode}"
     )
 
 # Using the @lru_cache() decorator on top,
