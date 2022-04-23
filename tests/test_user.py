be6ea7b648421275e5b7c7131b64283d0a934532
Victory Ifebhor <victory.ifebhor@gmail.com>
2022-04-23 11:18:07 +0100

    Removed the breakpoints in the test code

--- tests/test_user.py
+++ tests/test_user.py
@@ -5,18 +5,15 @@ client = get_testing_client()
 
 
 def test_login():
-    breakpoint()
     login = {
         "username": user["username"],
         "password": user["password"]
     }
-    breakpoint()
     response = client.post(
         "/user/auth/login",
         data=login
     )
     response_json = response.json()
-    breakpoint()
     assert response.status_code == 200
     assert "token" in response_json
     assert response_json["token_type"] == "Bearer"
