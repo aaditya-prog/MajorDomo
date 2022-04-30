4d884ae784221d75be8ccee1cea10cccb952c48f
Aaditya Dulal <artdityadulal@gmail.com>
2022-04-30 20:17:21 +0545

    Update Permissions and Message

--- app/routers/food.py
+++ app/routers/food.py
@@ -17,19 +17,19 @@ admin_router = APIRouter(dependencies=[Depends(ensure_is_admin)])
 
 
 # Get Food Categories
-@router.get("/categories/", response_model=list[str])
+@router.get("/categories/", response_model=list[str], dependencies=[Depends(ensure_is_admin)])
 def get_food_categories(db: Session = Depends(get_db)):
     return food_crud.get_category(db=db)
 
 
 # Show all food of food menu
-@router.get("/menu-list/", response_model=list[Food])
+@router.get("/menu-list/", response_model=list[Food], dependencies=[Depends(ensure_is_admin)])
 def get_food(db: Session = Depends(get_db)):
     return food_crud.get_food(db=db)
 
 
 # Show food by selected category
-@router.get("/menu-by-category/", response_model=FoodByCategory)
+@router.get("/menu-by-category/", response_model=FoodByCategory, dependencies=[Depends(ensure_is_admin)])
 def get_food_by_category(category: list[str] = Query(None), db: Session = Depends(get_db)):
     result: dict = {}
     for each_category in category:
@@ -39,19 +39,19 @@ def get_food_by_category(category: list[str] = Query(None), db: Session = Depend
 
 
 # Add new food item
-@admin_router.post("/menu/", response_model=Food)
+@admin_router.post("/menu/", response_model=Food, dependencies=[Depends(ensure_is_admin)])
 def create_food(new_food: FoodData, db: Session = Depends(get_db)):
     return food_crud.create_food(db=db, new_food=new_food)
 
 
 # Update food details
-@admin_router.put("/menu/{food_id}/", response_model=Food)
+@admin_router.put("/menu/{food_id}/", response_model=Food, dependencies=[Depends(ensure_is_admin)])
 def update_food(food_id: int, food: FoodData, db: Session = Depends(get_db)):
     return food_crud.update_food(db=db, food=food, food_id=food_id)
 
 
 # Delete food
-@admin_router.delete("/menu/{food_id}/")
+@admin_router.delete("/menu/{food_id}/", dependencies=[Depends(ensure_is_admin)])
 def delete_food(food_id: int, db: Session = Depends(get_db)):
     return food_crud.delete_food(db=db, food_id=food_id)
 
